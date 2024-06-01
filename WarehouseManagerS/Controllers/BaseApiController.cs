44a9b0a9f1dde46f178f5ce8e4422bce539517a6
Mateusz <matiz588@gmail.com>
2024-06-01 20:16:15 +0200

    BaseApiController & AccountController Added

---
+++ WarehouseManagerS/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagerS.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
