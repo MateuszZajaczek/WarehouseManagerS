36bac1914a9afcbcd6b9ea11ee5bb4749999de7d
Mateusz kicaj <matiz588@gmail.com>
2024-10-16 08:27:22 +0200

    updates

--- WarehouseManagerS/client/src/app/app.component.ts
+++ WarehouseManagerS/client/src/app/app.component.ts
@@ -7,13 +7,13 @@ import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css',],
-  imports: [RouterOutlet, NavComponent]
+  imports: [HomeComponent]
 })
 export class AppComponent implements OnInit {
   componentDestroyed = new Subject();
--- WarehouseManagerS/client/src/app/app.module.ts
+++ WarehouseManagerS/client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { ItemService } from './_services/item.service';
 import { NavComponent } from './nav/nav.component';
