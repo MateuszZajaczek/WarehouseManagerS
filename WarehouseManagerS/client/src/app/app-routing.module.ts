dc88856ce57579b305803b1bb49aa2f85a315388
Mateusz kicaj <matiz588@gmail.com>
2024-11-01 18:41:34 +0100

    route guards added

--- WarehouseManagerS/client/src/app/app-routing.module.ts
+++ WarehouseManagerS/client/src/app/app-routing.module.ts
@@ -4,16 +4,25 @@ import { HomeComponent } from './_components/home/home.component';
 import { ItemListComponent } from './_components/ItemsList/item-list.component';
 import { ReturnsListComponent } from './_components/returns-list/returns-list.component';
 import { OrdersListComponent } from './_components/orders-list/orders-list.component';
+import { authGuard } from './_guard/auth.guard';
 // Importuj inne komponenty według potrzeb
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Ścieżka do strony głównej
-  { path: 'items', component: ItemListComponent }, // Ścieżka do listy przedmiotów
-  { path: 'orders', component: OrdersListComponent }, // Ścieżka do listy zamówień
-  { path: 'returns', component: ReturnsListComponent }, // Ścieżka do listy zwrotów
-  { path: '**', component: HomeComponent, pathMatch: 'full'}, //Ścieżka do strony głównej
-  ];
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'items', component: ItemListComponent }, // Ścieżka do listy przedmiotów
+      { path: 'orders', component: OrdersListComponent }, // Ścieżka do listy zamówień
+      { path: 'returns', component: ReturnsListComponent }, // Ścieżka do listy zwrotów
+    ]
+  },
+
+  { path: '**', component: HomeComponent, pathMatch: 'full' }, //Ścieżka do strony głównej
+];
 
 
 @NgModule({
